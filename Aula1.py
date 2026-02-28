123c6d478767d594e14e0a0d787fbb1d19d58377
JPSilva2008 <jpbruca@icloud.com>
2026-02-28 18:25:49 -0300

    Add files via upload

---
+++ Aula1.py
@@ -0,0 +1,8 @@
+# print(type(3.6))  = float = Número Real
+# print(type(2))   =  int = Número  Inteiro
+# print(type("ola")) =  str =  Palavra ou Frase
+# print(type(3+6j)) = complex = Número Complexo
+# print = Função de impressão 
+# %d = marcador , %f = marcador de Números decimais, %s = marcador de string
+# input = Função de entrada de dados
+
