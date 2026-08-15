35984a78dce22d2eb7bf4db82147430a1d549f17
JPSilva2008 <jpbruca@icloud.com>
2026-08-15 18:07:31 -0300

    Delete Aula61.py

--- Aula61.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Dgite um numero: "))
-acumulador = 0
-qtd_numeros = 0
-while(n!=0):
-    acumulador += n
-    qtd_numeros +=1
-    n = int(input("Dgite um numero: "))
-print(acumulador/qtd_numeros)
